73a260e8d32f43d6a01565bdaf3b6e993bf74d7f
Jure Žvelc <jzvelc@gmail.com>
2014-06-05 18:24:49 +0200

    Updated framework call.

--- app.coffee
+++ app.coffee
@@ -1,3 +1,3 @@
 path = require 'path'
 src = path.normalize("#{__dirname}/src/server")
-module.exports = require('meanstack-framework').project(__dirname, src, '.coffee')
+module.exports = require('meanstack-framework')(__dirname, src, '.coffee')
--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var path = require('path');
 var src = path.normalize(__dirname + '/lib/server');
-module.exports = require('meanstack-framework').project(__dirname, src, '.js');
+module.exports = require('meanstack-framework')(__dirname, src, '.js');
